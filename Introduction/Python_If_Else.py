2b13be3220cdb5338370569a2fada967c52e3668
Katreddi Sri Sai Durga <72980202+KATREDDIDURGA@users.noreply.github.com>
2021-11-16 15:04:10 +0530

    Update Python_If_Else.py

--- Introduction/Python_If_Else.py
+++ Introduction/Python_If_Else.py
@@ -1,4 +1,17 @@
 #!/bin/python
+#If  is odd, print Weird
+#If  is even and in the inclusive range of  to , print Not Weird
+#If  is even and in the inclusive range of  to , print Weird
+#If  is even and greater than , print Not Weird
+#Input Format
+
+#A single line containing a positive integer, .
+
+#Constraints
+
+#Output Format
+
+#Print Weird if the number is weird. Otherwise, print Not Weird.
 
 import math
 import os
