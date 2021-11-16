cf8b989bc881384fdd703bf147790af5ff1af80b
Katreddi Sri Sai Durga <72980202+KATREDDIDURGA@users.noreply.github.com>
2021-11-16 15:07:57 +0530

    Create Loops.py

---
+++ Introduction/Loops.py
@@ -0,0 +1,9 @@
+#The provided code stub reads and integer, , from STDIN. For all non-negative integers , print .
+
+
+if __name__ == '__main__':
+    n = int(raw_input())
+    i=0
+    while i<n:
+        print(i*i)
+        i=i+1
